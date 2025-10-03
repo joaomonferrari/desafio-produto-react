a1504fd1fabb88f65ede1be5de496e2ce1c815c5
joaomonferrari <sherlockholomes15@gmail.com>
2025-10-03 13:43:02 -0300

    deploy: ajustes Vite base + workflows

--- frontend-react/vite.config.js
+++ frontend-react/vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/desafio-produto-react/',
   plugins: [react()],
   server: {
-    
-    proxy: {
-      '/api': 'http://localhost:5000'
-    }
+    proxy: { '/api': 'http://localhost:5000' } // para desenvolvimento local
   }
 })
